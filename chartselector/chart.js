58ed1149c75306ba1bf0bf9028f119f712f944fb
hassan ijaz <rai.hasan@gmail.com>
2017-02-12 07:52:27 -0500

    init chartselector

---
+++ chartselector/chart.js
@@ -0,0 +1,143 @@
+r Chart',
+  'atype':['Comparison']
+},
+{'id':'Column Chart',
+  'atype':['Comparison']
+},
+{'id':'Table of Table with embedded charts',
+  'atype':['Comparison']
+},
+{'id':'Variable Width Column Chart',
+  'atype':['Comparison']
+},
+{'id':'Circular Area Chart',
+  'atype':['Comparison']
+},
+{'id':'Line Chart',
+  'atype':['Comparison']
+},
+{'id':'Column Chart',
+  'atype':['Comparison']
+},
+{'id':'Column Histogram',
+  'atype':['Distribution']
+},
+{'id':'Line Histogram',
+  'atype':['Distribution']
+},
+{'id':'Scatter Chart',
+  'atype':['Distribution','Relationship/Correlation']
+},
+{'id':'3D Area Chart',
+  'atype':['Distribution']
+},
+{'id':'Stacked 100% Column Chart with SubComponents',
+  'atype':['Composition']
+},
+{'id':'Waterfall Chart',
+  'atype':['Composition']
+},
+{'id':'Pie Chart',
+  'atype':['Composition']
+},
+{'id':'Stacked Area Chart',
+  'atype':['Composition']
+},
+{'id':'Stacked 100% Area Chart',
+  'atype':['Composition']
+},
+{'id':'Stacked Column Chart',
+  'atype':['Composition']
+},
+{'id':'Stacked 100% Column Chart',
+  'atype':['Composition']
+},
+{'id':'Bubble Chart',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Box Plot Vertical',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Box Plot Horizontal',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Strip Plot Single',
+  'atype':['Distribution']
+},
+{'id':'Strip Plot Multiple',
+  'atype':['Distribution']
+},
+{'id':'Dot Plot Horizontal',
+  'atype':['Ranking']
+},
+{'id':'Dot Plot Vertical',
+  'atype':['Ranking']
+},
+{'id':'Heat Map',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Correlogram',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Bullet Chart',
+  'atype':['Progress']
+},
+{'id':'Guage Chart',
+  'atype':['Progress']
+},
+{'id':'Map',
+  'atype':['Spatial']
+},
+{'id':'Hierarchical Dendrogram',
+  'atype':['Composition']
+},
+{'id':'Calendar heatmap',
+  'atype':['Relationship/Correlation']
+},
+{'id':'Waffle Chart',
+  'atype':['Composition']
+},
+{'id':'Treemap',
+  'atype':['Composition']
+},
+{'id':'Density Plot',
+  'atype':['Distribution']
+},
+{'id':'Violin Plot',
+  'atype':['Distribution']
+},
+{'id':'Population pyramid',
+  'atype':['Distribution','Composition']
+},
+{'id':'Ordered bar chart',
+  'atype':[]
+},
+{'id':'Lollipop chart',
+  'atype':['Ranking']
+},
+{'id':'Slope Chart',
+  'atype':['Ranking']
+},
+{'id':'Dumbbell Plot',
+  'atype':['Ranking']
+},
+{'id':'Diverging Bars',
+  'atype':['Ranking']
+},
+{'id':'Diverging Lollipop Chart',
+  'atype':['Deviation']
+},
+{'id':'Diverging Dot Plot',
+  'atype':['Deviation']
+},
+{'id':'Area Chart',
+  'atype':['Deviation']
+},
+{'id':'Marginal Histogram / Boxplot',
+  'atype':['Relationship/Correlation','Deviation']
+},
+{'id':'Jitter Plot',
+  'atype':['Relationship/Correlation']
+}
+]
+
