a9628af65c1fa7499d3df6cb4c44eccc9c80ffaf
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-03-09 22:06:24 -0500

    Update data.js

--- chartselector/data.js
+++ chartselector/data.js
@@ -1,7 +1,27 @@
 {
-  "question":"Analysis Type?",
-  "options":[
-    "Composition":{},
+  "q":"Analysis Type?",
+  "o":[
+    "Composition":{
+      "q":"Visualize change over time?",
+      "o":[
+        "Yes":{
+          "q":"Relative or absolute composition?",
+          "o":[
+            "Relative":{
+              "q":"Continuous or discrete data?"
+              "o":[
+                "Continuous":{
+                  "q":"Use a <a href='https://www.google.com/search?q=Stacked+100%25+area+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjjt8ybzbnSAhUIiVQKHUlmAzAQ_AUICCgB&amp;biw=1440&amp;bih=803'>Stacked 100% Area Chart</a>"
+                },
+                "Discrete":{}
+              ]
+            },
+            "Absolute":{}
+          ]
+        },
+        "No":{}
+      ]
+    },
     "Relationship":{},
     "Comparison":{},
     "Distribution":{},
