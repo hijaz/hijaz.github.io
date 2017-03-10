1626a4222b0e976e208ae79519f658fd61005a5a
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-03-09 22:53:47 -0500

    Update data.js

--- chartselector/data.js
+++ chartselector/data.js
@@ -1,39 +1,39 @@
 {
   "q":"Analysis Type?",
-  "o":[
+  "o":{
     "Composition":{
       "q":"Visualize change over time?",
-      "o":[
+      "o":{
         "Yes":{
           "q":"Relative or absolute composition?",
-          "o":[
+          "o":{
             "Relative":{
               "q":"Continuous or discrete data?",
-              "o":[
+              "o":{
                 "Continuous":{
                   "q":"Use a <a href='https://www.google.com/search?q=Stacked+100%25+area+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjjt8ybzbnSAhUIiVQKHUlmAzAQ_AUICCgB&amp;biw=1440&amp;bih=803'>Stacked 100% Area Chart</a>"
                 },
                 "Discrete":{
                   "q":"Use a <a href='https://www.google.com/search?q=Stacked+100%25+area+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjjt8ybzbnSAhUIiVQKHUlmAzAQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=Stacked+100%25+Column+Chart&amp;*'>Stacked 100% Column Chart</a>"
                 }
-              ]
+              }
             },
             "Absolute":{
              "q":"Continuous or discrete data?",
-              "o":[
+              "o":{
                 "Continuous":{
                   "q":"Use a <a href='https://www.google.com/search?q=Stacked+100%25+area+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjjt8ybzbnSAhUIiVQKHUlmAzAQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=stacked+area+chart&amp;*'>Stacked Area Chart</a>"
                 },
                 "Discrete":{
                   "q":"Use a <a href='https://www.google.com/search?q=Stacked+100%25+area+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjjt8ybzbnSAhUIiVQKHUlmAzAQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=Stacked+Column+Chart&amp;*'>Stacked Column Chart</a>"
                 }
-              ]
+              }
             }
-          ]
+          }
         },
         "No":{
           "q":"Choose one:",
-          "o":[
+          "o":{
             "Heirarchy":{
               "q":"Use a <a href='https://www.google.com/search?q=tree+map&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwip757g1LnSAhWIqlQKHepAA_UQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=treemap&amp;*'>Tree Map</a> or a <a href='https://www.google.com/search?q=tree+map&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwip757g1LnSAhWIqlQKHepAA_UQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=Heirarchial+Dendogram&amp;*'>Heirarchial Dendogram</a>"
             },
@@ -46,65 +46,65 @@
             "Simple state of the total":{
               "q":"Use a <a href='https://www.google.com/search?q=tree+map&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwip757g1LnSAhWIqlQKHepAA_UQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=pie+chart&amp;*'>Pie Chart</a> or a <a href='https://www.google.com/search?q=tree+map&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwip757g1LnSAhWIqlQKHepAA_UQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=waffle+chart&amp;*'>Waffle Chart</a>"
             }
-          ] 
+          } 
         }
-      ]
+      }
     },
     "Relationship":{},
     "Comparison":{
       "q":"Visualize change over time?",
-      "o":[
+      "o":{
         "Yes":{
           "q":"Discrete or Continuous?",
-          "o":[
+          "o":{
             "Discrete":{
               "q":"Number of variables?",
-              "o":[
+              "o":{
                 "One":{
                   "q":"Use a <a href='https://www.google.com/search?q=bar+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiAgd2R_sXSAhVGSiYKHcUBAU0Q_AUICCgB&amp;biw=1440&amp;bih=803'>Bar Chart</a>"
                 },
                 "Two":{
                   "q":"Use a <a href='https://www.google.com/search?q=bar+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiAgd2R_sXSAhVGSiYKHcUBAU0Q_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=variable+width+bar+chart&amp;*'>Variable width bar chart</a>"
                 }
-              ]
+              }
             },
             "Continuous":{
               "q":"Cyclical time?",
-              "o":[
+              "o":{
                 "Cyclical":{
                   "q":"Use a <a href='https://www.google.com/search?q=circular+line+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiEsPvY_8XSAhWB0iYKHVSMAjQQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=circular+area+chart&amp;*'>Circular Area Chart</a>"
                 },
                 "Linear":{
                   "q":"Use a <a href='https://www.google.com/search?q=circular+line+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiEsPvY_8XSAhWB0iYKHVSMAjQQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=line+chart&amp;*'>Line Chart</a>"
                 }
-              ]
+              }
             }
-          ]
+          }
         },
         "No":{
           "q":"Categories to compare",
-          "o":[
+          "o":{
             "Few":{
               "q":"Number of variables",
-              "o":[
+              "o":{
                 "One":{
                   "q":"Use a <a href='https://www.google.com/search?q=bar+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiAgd2R_sXSAhVGSiYKHcUBAU0Q_AUICCgB&amp;biw=1440&amp;bih=803'>Bar Chart</a>"
                 },
                 "Two":{
                   "q":"Use a <a href='https://www.google.com/search?q=bar+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiAgd2R_sXSAhVGSiYKHcUBAU0Q_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=variable+width+bar+chart&amp;*'>Variable width bar chart</a>"
                 }
-              ]
+              }
             },
             "Many":{
               "q":"Use <a href='https://www.google.com/search?q=circular+line+chart&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiEsPvY_8XSAhWB0iYKHVSMAjQQ_AUICCgB&amp;biw=1440&amp;bih=803#tbm=isch&amp;q=ggplot+facets&amp;*'>facets</a>"
             }
-          ]  
+          }  
         }
-      ]
+      }
     },
     "Distribution":{},
     "Ranking":{},
     "Progress":{},
     "Deviation":{}
-  ]
+  }
 }
