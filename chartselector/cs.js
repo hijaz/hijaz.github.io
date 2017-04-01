dbd10e43b80522333b60e6edaa85fdcf240fb0d4
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-04-01 00:49:24 -0400

    Update cs.js

--- chartselector/cs.js
+++ chartselector/cs.js
@@ -1,4 +1,10 @@
 
 jQuery(document).ready(function(){
   console.log(data);
+
+  //		<button type="button" class="q btn btn-default">Button</button> 
+
+  $(document).on("click", ".q", function(event){
+    alert($(this).text());
+  });
 });
