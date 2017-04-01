23c94f2c69915c8c99cd808e063fd732090cec2c
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-04-01 01:07:50 -0400

    Update cs.js

--- chartselector/cs.js
+++ chartselector/cs.js
@@ -2,9 +2,19 @@
 jQuery(document).ready(function(){
   console.log(data);
 
-  //		<button type="button" class="q btn btn-default">Button</button> 
-
+  //<button type="button" class="q btn btn-default">Button</button> 
+  var stack = [];
+  var cur = data;
+  var $text = jQuery('#text');
+  var $btns = jQuery('#btns');
+  var update = function(){
+    $text.text(cur.q);
+    
+  }
+  update();
   $(document).on("click", ".q", function(event){
     alert($(this).text());
   });
+  
+  
 });
