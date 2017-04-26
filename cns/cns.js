538da6d32351749661cdcd050324d9f8de77c04c
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-04-26 01:41:46 -0400

    Update cns.js

--- cns/cns.js
+++ cns/cns.js
@@ -1,36 +1,50 @@
 var cns = {};
+cns.is_logged_in = false;
+cns.config = {
+    apiKey: "AIzaSyB9B968uA15pTg_VpJS1VoApHjJJhWlRtc",
+    authDomain: "cands-fcd62.firebaseapp.com",
+    databaseURL: "https://cands-fcd62.firebaseio.com/",
+    storageBucket: "bucket.appspot.com"
+};
 
-//auth
-cns.auth_google_provider = new firebase.auth.GoogleAuthProvider();
+//login flow
+cns.login_flow = function(){
+  //is user signed in
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      cns.is_logged_in = true;
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        cns.user = result.user;
+        
+        //init app & access db
+        firebase.initializeApp(cns.config);
+        cns.database = firebase.database();
 
-//is logged in?
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
         // ...
-      }
-      // The signed-in user info.
-      var user = result.user;
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-  } else {
-    // No user is signed in.
-    //redirect to login
-    firebase.auth().signInWithRedirect(cns.auth_google_provider);
-  }
-});
+      });
+    } else {
+      // No user is signed in.
+      //redirect to login
+      cns.auth_google_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(cns.auth_google_provider);
+    }
+  });
+};
 
 //signout
 /**
@@ -39,19 +53,4 @@ firebase.auth().signOut().then(function() {
 }).catch(function(error) {
   // An error happened.
 });
-**/
-
-/**
-  // Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
-    apiKey: "AIzaSyB9B968uA15pTg_VpJS1VoApHjJJhWlRtc",
-    authDomain: "cands-fcd62.firebaseapp.com",
-    databaseURL: "https://cands-fcd62.firebaseio.com/",
-    storageBucket: "bucket.appspot.com"
-  };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
 **/
