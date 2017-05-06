0039271b8bc1918c923e1e5cfb7c6ba281ccf935
Hassan Ijaz <hijaz@users.noreply.github.com>
2017-05-05 21:05:45 -0400

    Update cns.js

--- cns/cns.js
+++ cns/cns.js
@@ -17,7 +17,8 @@ cns.login_flow = function(){
       jQuery('#login_img').attr('src',cns.user.photoURL);
       //access db
       cns.database = firebase.database();  
-
+      //init data
+      cns.db.init();
     } else {
       // No user is signed in.
       jQuery('#login_details').hide();
@@ -43,52 +44,52 @@ cns.log_out = function(){
 };
 
 //data
-cns.data={};
+cns.db.data={};
 
 //write to db
-cns.data.write = function () {
+cns.db.write = function () {
   var userId = firebase.auth().currentUser.uid;
-  return firebase.database().ref('users/' + userId).set(cns.data);
+  return firebase.database().ref('users/' + userId).set(cns.db.data);
 }
 
 //read from db once 
-cns.data.read = function(){
+cns.db.read = function(){
   var userId = firebase.auth().currentUser.uid;
   return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
     //var username = snapshot.val().username;
-    cns.data= snapshot.val();
+    cns.db.data= snapshot.val();
   });
 }
 
 //init db
-cns.data.init = function(){
+cns.db.init = function(){
   var userId = firebase.auth().currentUser.uid;
-  cns.data.read();
-  if(cns.data.timestamp){
+  cns.db.read();
+  if(cns.db.data.timestamp){
     //existing user
   }else{
     //first time user
-    cns.data.timestamp = new Date().getTime();
-    cns.data.habits = {
+    cns.db.data.timestamp = new Date().getTime();
+    cns.db.data.habits = {
                       'Sample Habit - Workout':{
                         'good':true,
                         'carrots': 2,
                         'sticks': 2
                       }
                      };
-    cns.data.carrots = {
+    cns.db.data.carrots = {
                   'Sample Carrot - Pizza':{
                     'unit':'slice',
                     'price': 4
                   }
                  };
-    cns.data.sticks = {
+    cns.db.data.sticks = {
                   'Sample Stick - Squats':{
                     'unit': 'rep',
                     'price': 1
                   }
                  };
-    cns.data.write();
+    cns.db.write();
   }
 }
 
